61be63778b36144fa79757a2d09b48d64e53fca6
turim0 <ivan_medvedev_1996@mail.ru>
2022-04-17 16:48:00 +0300

    setup DialogFlow

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+env = Env()
+env.read_env()
+
 
 def start(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
@@ -28,14 +31,42 @@ def echo(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(update.message.text)
 
 
+def detect_intent_texts(update: Update, context: CallbackContext):
+    project_id = env('PROJECT_ID')
+    language = env('BOT_LANGUAGE')
+    from google.cloud import dialogflow
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, update.effective_user.id)
+    print("Session path: {}\n".format(session))
+
+    text_input = dialogflow.TextInput(text=update.message.text, language_code=language)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    print("=" * 20)
+    print("Query text: {}".format(response.query_result.query_text))
+    print(
+        "Detected intent: {} (confidence: {})\n".format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence,
+        )
+    )
+    print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
+    update.message.reply_text(response.query_result.fulfillment_text)
+
+
 def main():
-    env = Env()
-    env.read_env()
     updater = Updater(env('TELEGRAM_BOT_TOKEN'))
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, detect_intent_texts))
     updater.start_polling()
     updater.idle()
 
