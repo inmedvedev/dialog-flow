929d48b6bcdb7fba335f0ec4a4dc86261c432a49
turim0 <ivan_medvedev_1996@mail.ru>
2022-04-19 00:45:34 +0300

    Add logging

--- learning.py
+++ learning.py
@@ -1,6 +1,9 @@
 from google.cloud import dialogflow
+from google.api_core.exceptions import InvalidArgument
 from environs import Env
+import argparse
 import json
+import logging
 
 
 def create_intent(project_id, display_name, training_phrases_parts, message_texts):
@@ -28,7 +31,15 @@ if __name__ == '__main__':
     env = Env()
     env.read_env()
     project_id = env('PROJECT_ID')
-    with open('questions.json', 'r', encoding='utf-8') as file:
-        file_contents = json.load(file)
-    for intent_name, training_data in file_contents.items():
-        create_intent(project_id, intent_name, training_data['questions'], training_data['answer'])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filename', default='questions.json')
+    args = parser.parse_args()
+    logger = logging.getLogger('learning')
+    try:
+        with open(args.filename, 'r', encoding='utf-8') as file:
+            file_contents = json.load(file)
+        for intent_name, training_data in file_contents.items():
+            create_intent(project_id, intent_name, training_data['questions'], training_data['answer'])
+    except Exception as error:
+        logger.exception(error)
+
